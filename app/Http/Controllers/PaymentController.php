099a82fb405bf252e2e9432aa811c44f1fdd9b4a
Enrique Garcia <enramgarcia@gmail.com>
2023-04-26 13:16:32 -0500

    Ruta de pago faltante

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,7 +2,19 @@
 
 namespace App\Http\Controllers;
 
-class PaymentController
-{
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
+class PaymentController extends Controller
+{
+    /**
+     * Esta funcion es meramente demostrativa y no tiene uso.
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request): Response
+    {
+        return Inertia::render('Payment');
+    }
 }
--- resources/js/Pages/Payment.vue
+++ resources/js/Pages/Payment.vue
@@ -1,13 +1,73 @@
-<template>
-$END$
-</template>
-
-<script>
-export default {
-name: "Payment"
-}
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
 </script>
 
-<style scoped>
+<template>
+    <Head title="Dashboard" />
 
-</style>
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Pagos</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    <form>
+                        <div class="space-y-12">
+                            <div class="border-b border-gray-900/10 pb-12">
+                                <h2 class="text-base font-semibold leading-7 text-gray-900">Pago de Cuota</h2>
+                                <p class="mt-1 text-sm leading-6 text-gray-600">Ingrese los datos para pagar su cuota (Esto es demostrativo)</p>
+
+                                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+
+                                    <div class="col-span-full">
+                                        <label for="cardholdername" class="block text-sm font-medium leading-6 text-gray-900">Tarjetahabiente</label>
+                                        <div class="mt-2">
+                                            <input type="text" name="cardholdername" id="cardholdername" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-span-full">
+                                        <label for="card" class="block text-sm font-medium leading-6 text-gray-900">Tarjeta</label>
+                                        <div class="mt-2">
+                                            <input type="password" name="card" id="card" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                        </div>
+                                    </div>
+
+                                    <div class="sm:col-span-2 sm:col-start-1">
+                                        <label for="expiration_date" class="block text-sm font-medium leading-6 text-gray-900">Fecha Expiracion</label>
+                                        <div class="mt-2">
+                                            <input type="text" name="expiration_date" id="expiration_date" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                        </div>
+                                    </div>
+
+                                    <div class="sm:col-span-2">
+                                        <label for="ccv" class="block text-sm font-medium leading-6 text-gray-900">CCV</label>
+                                        <div class="mt-2">
+                                            <input type="text" name="ccv" id="ccv" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-span-full">
+                                        <label for="street-address" class="block text-sm font-medium leading-6 text-gray-900">Street address</label>
+                                        <div class="mt-2">
+                                            <input type="text" name="street-address" id="street-address" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="mt-6 flex items-center justify-end gap-x-6">
+                            <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancelar</button>
+                            <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Pagar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
